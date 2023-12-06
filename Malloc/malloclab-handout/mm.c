50be9606a47e403a32e2c02bc3fbeb1c0289ff25
chenzongyao200127 <2319251792@qq.com>
2023-12-06 18:28:43 +0800

    finish:     static void *extend_heap(size_t words);     int mm_check(void);     static void add_to_free_list(char *bp);     static int is_in_free_list(char *bp);     static void remove_from_free_list(char* bp);     static void *coalesce(char *bp);

--- Malloc/malloclab-handout/mm.c
+++ Malloc/malloclab-handout/mm.c
@@ -28,7 +28,7 @@ team_t team = {
     /* First member's full name */
     "Chen Zongyao",
     /* First member's email address */
-    "Solar1s@seu.edu.cn",
+    "solar1s@seu.edu.cn",
     /* Second member's full name (leave blank if none) */
     "",
     /* Second member's email address (leave blank if none) */
@@ -38,18 +38,226 @@ team_t team = {
 /* single word (4) or double word (8) alignment */
 #define ALIGNMENT 8
 
+/* Basic constants and macros */
+#define WSIZE 4     /* Word and header/footer size (bytes) */
+#define DSIZE 8     /* Double word size (bytes) */
+#define CHUNKSIZE (1<<12)   /* Extend heap by this amont (bytes) */
+
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+
+/* Pack and size and allocated bit into a word */
+#define PACK(size, alloc) ((size) | (alloc))
+
+/* Read and write a word at adderss p */
+#define GET(p)       (*(unsigned int *)(p))            
+#define PUT(p, val)  (*(unsigned int *)(p) = (val))    
+
+/* Read the size and allocated fields from address p */
+#define GET_SIZE(p)     (GET(p) & (~0x7))
+#define GET_ALLOC(p)    (GET(p) & 0x1)
+
+/* Given block ptr bp, compute address of its header and footer */
+#define HDRP(bp)        ((char *)(bp) - WSIZE)
+#define FTRP(bp)        ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
+
+/* Given block ptr bp, compute address of next and previous blocks */
+#define NEXT_BLKP(bp)   ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE)))
+#define PREV_BLKP(bp)   ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))
+
+/* Give free block ptr bp, get the val of `pred` and `succ` of the free block */
+#define GET_PRED(p)  (*(void **)(p))
+#define GET_SUCC(p)  (*(void **)((p) + WSIZE))
+
+/* Give free block ptr bp, set the val of `pred` and `succ` of the free block */
+#define SET_PRED(p, pred)  (*(void **)(p) = (pred))
+#define SET_SUCC(p, succ)  (*(void **)((p) + WSIZE) = (succ))
+
 /* rounds up to the nearest multiple of ALIGNMENT */
 #define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
 
-
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
+
+/* Global variables */
+static char *heap_listp = 0;        /* Pointer to first block */  
+static char *dummy_blockp = 0;      /* Pointer to first dummy free block */
+
+/* Function prototypes for internal helper routines */
+static void *extend_heap(size_t words);
+int mm_check(void);
+static void add_to_free_list(char *bp);
+static int is_in_free_list(char *bp);
+static void remove_from_free_list(char* bp);
+static void *coalesce(char *bp);
+
 
 /* 
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
 {
+    /* Initialize the heap list pointer to create an empty heap. 
+       Allocate memory space for the additional blocks, checking for failure. */
+    if ((heap_listp = mem_sbrk((4 + 2) * WSIZE)) == (void *) - 1) // Allocate space for dummy block and its pointers
+        return -1;
+
+    /* Set up the alignment padding at the beginning of the heap. */
+    PUT(heap_listp, 0);                           
+
+    /* Initialize the Prologue */
+    PUT(heap_listp + (1 * WSIZE), PACK(DSIZE, 1)); 
+    PUT(heap_listp + (2 * WSIZE), PACK(DSIZE, 1)); 
+
+    /* Initialize the dummy block. */
+    PUT(heap_listp + (3 * WSIZE), PACK(DSIZE, 1)); // Dummy block header
+    PUT(heap_listp + (4 * WSIZE), 0); // Dummy block predecessor pointer (null)
+    PUT(heap_listp + (5 * WSIZE), 0); // Dummy block successor pointer (null)
+    PUT(heap_listp + (6 * WSIZE), PACK(DSIZE, 1)); // Dummy block footer
+
+    dummy_blockp = heap_listp + (4 * WSIZE); // set global dummy pointer
+
+    /* Set up the epilogue header, which is now two blocks further. */
+    PUT(heap_listp + (7 * WSIZE), PACK(0, 1));     
+
+    /* Move heap list pointer past the prologue and dummy block for future allocations. */
+    heap_listp += (6 * WSIZE);
+
+    /* Extend the heap with a free block of size CHUNKSIZE. */
+    if (extend_heap(CHUNKSIZE / WSIZE) == NULL)
+        return -1;
+
+    /* If everything is successful, return 0 indicating successful initialization. */
     return 0;
+}
+
+/*
+* extend_heap - extend heap CHUNKSIZE bytes, Initialize free block and the epilogue header
+*/
+static void *extend_heap(size_t words) {
+    char *bp;
+    size_t size;
+
+    /* Allocate an even number of words to maintain alignment */
+    size = (words % 2) ? (words + 1) * WSIZE : words * WSIZE;
+    if ((long)(bp = mem_sbrk(size)) == -1)
+        return NULL;
+
+    /* Initialize free block header/footer and the epilogue header */
+    PUT(HDRP(bp), PACK(size, 0));         /* Free block header */
+    PUT(FTRP(bp), PACK(size, 0));         /* Free block footer */
+    PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1)); /* New epilogue header */
+
+    /* Add the new free block to the free list */
+    add_to_free_list(bp);
+
+    /* Coalesce if the previous block was free */
+    return coalesce(bp);
+}
+
+/*
+* mm_check - debug tool
+*/
+int mm_check(void) {
+    char *bp = heap_listp;
+
+    /* Check all blocks in the heap */
+    while (GET_SIZE(HDRP(bp)) != 0) {
+        if (!is_valid_heap_address(bp)) {
+            printf("Line %d: Invalid heap address found at %p\n", __LINE__, bp);
+            return 0;
+        }
+
+        if (GET_ALLOC(HDRP(bp)) && GET_ALLOC(NEXT_BLKP(bp))) {
+            if (FTRP(bp) > HDRP(NEXT_BLKP(bp))) {
+                printf("Line %d: Overlapping allocated blocks found.\n", __LINE__);
+                return 0;
+            }
+        }
+
+        bp = NEXT_BLKP(bp);
+    }
+
+    /* Check all blocks in the free list */
+    for (bp = GET_SUCC(dummy_blockp); bp != NULL; bp = GET_SUCC(bp)) {
+        if (GET_ALLOC(bp)) {
+            printf("Line %d: Allocated block found in free list at %p\n", __LINE__, bp);
+            return 0;
+        }
+
+        if (!is_valid_heap_address(bp) || GET_ALLOC(bp)) {
+            printf("Line %d: Invalid or allocated block in free list at %p\n", __LINE__, bp);
+            return 0;
+        }
+
+        if (!GET_ALLOC(bp) && !GET_ALLOC(PREV_BLKP(bp))) {
+            printf("Line %d: Contiguous free blocks that escaped coalescing found.\n", __LINE__);
+            return 0;
+        }
+    }
+
+    /* Check if every free block is actually in the free list */
+    for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)) {
+        if (!GET_ALLOC(bp) && !is_in_free_list(bp)) {
+            printf("Line %d: Free block not in free list found at %p\n", __LINE__, bp);
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+
+
+/* 
+ * add_to_free_list - give a free block pointer bp, add this block to the LIFO queue
+ */
+static void add_to_free_list(char *bp) {
+    /* Set the successor of the new block to be the current successor of the dummy block */
+    SET_SUCC(bp, GET_SUCC(dummy_blockp));
+
+    /* Set the predecessor of the new block to be the dummy block */
+    SET_PRED(bp, dummy_blockp);
+
+    /* If the dummy block's successor is not NULL, update its predecessor to the new block */
+    if (GET_SUCC(dummy_blockp) != NULL) {
+        SET_PRED(GET_SUCC(dummy_blockp), bp);
+    }
+
+    /* Update the dummy block's successor to the new block */
+    SET_SUCC(dummy_blockp, bp);
+}
+
+/* 
+ * is_in_free_list - give a free block pointer bp, check whether it is in the free list
+ */
+static int is_in_free_list(char *bp) {
+    char *current_bp = GET_SUCC(dummy_blockp); // Start from the first block in the free list
+
+    // Traverse the free list
+    while (current_bp != NULL) {
+        if (current_bp == bp) {
+            return 1; // Found the block in the free list
+        }
+        current_bp = GET_SUCC(current_bp); // Move to the next block in the free list
+    }
+
+    return 0; // Block not found in the free list
+}
+
+/*
+ * remove_from_free_list - given a free block pointer bp, remove it from the free block list
+ */
+static void remove_from_free_list(char* bp) {
+    // get predecessor block pointer
+    char* pred_bp = GET_PRED(bp);
+
+    // get successor block pointer
+    char* succ_bp = GET_SUCC(bp);
+
+    // update the successor of the predecessor block
+    SET_SUCC(pred_bp, succ_bp);
+
+    // update the predecessor of the successor block
+    SET_PRED(succ_bp, pred_bp);
 }
 
 /* 
@@ -71,9 +279,67 @@ void *mm_malloc(size_t size)
 /*
  * mm_free - Freeing a block does nothing.
  */
-void mm_free(void *ptr)
+void mm_free(void *bp)
 {
+    if (bp == 0) 
+        return;
+
+    size_t size = GET_SIZE(HDRP(bp));
+
+    if (heap_listp == 0){
+        mm_init();
+    }
+
+    PUT(HDRP(bp), PACK(size, 0));
+    PUT(FTRP(bp), PACK(size, 0));
+    coalesce(bp);
 }
+
+/*
+ * coalesce - Boundary tag coalescing. Return ptr to coalesced block
+ */
+static void *coalesce(char *bp) {
+    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+    size_t size = GET_SIZE(HDRP(bp));
+
+    /* Case 1: No coalescing */
+    if (prev_alloc && next_alloc) {
+        add_to_free_list(bp);
+        return bp;
+    }
+
+    /* Case 2: Coalesce with next block */
+    if (prev_alloc && !next_alloc) {
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+        remove_from_free_list(NEXT_BLKP(bp));
+        PUT(HDRP(bp), PACK(size, 0));
+        PUT(FTRP(bp), PACK(size, 0));
+    }
+
+    /* Case 3: Coalesce with previous block */
+    else if (!prev_alloc && next_alloc) {
+        size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+        bp = PREV_BLKP(bp);
+        remove_from_free_list(bp);
+        PUT(FTRP(bp), PACK(size, 0));
+        PUT(HDRP(bp), PACK(size, 0));
+    }
+
+    /* Case 4: Coalesce with both previous and next blocks */
+    else {
+        size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(FTRP(NEXT_BLKP(bp)));
+        remove_from_free_list(PREV_BLKP(bp));
+        remove_from_free_list(NEXT_BLKP(bp));
+        bp = PREV_BLKP(bp);
+        PUT(HDRP(bp), PACK(size, 0));
+        PUT(FTRP(bp), PACK(size, 0));
+    }
+
+    add_to_free_list(bp);
+    return bp;
+}
+
 
 /*
  * mm_realloc - Implemented simply in terms of mm_malloc and mm_free
@@ -94,17 +360,3 @@ void *mm_realloc(void *ptr, size_t size)
     mm_free(oldptr);
     return newptr;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ Malloc/note.md
@@ -0,0 +1,215 @@
+# Heap Checker 
+- Part of the assignment is writing a heap checker
+    1. This is here to help you.
+    2. Write the heap checker as you go, don't think of it as something to doat the end
+    3. A good heap checker will make debugging much, much easier
+- Heap checker tips
+    1. Heap checker should run silently until it finds an error
+        - Otherwise you will get more output than is useful
+        - You might find it useful to add a “verbose” flag, however
+    2. Consider using a macro to turn the heap checker on and off
+        - This way you don't have to edit all of the places you call it
+    3. There is a built-in macro called `__LINE__` that gets replaced withthe line number it's on
+        - You can use this to make the heap checker tell you where it failed
+
+GPT-4:
+Certainly! To complete the `mm_check` function, I'll provide a more detailed implementation with all the necessary checks based on a typical heap structure. Note that the exact implementation details may vary based on your specific memory management design, such as how you structure your heap blocks, maintain free lists, and manage heap boundaries.
+
+### Example Implementation of `mm_check`
+
+```c
+#include <stdio.h>
+#include <assert.h>
+
+// Define your block structure and global variables here
+// For example:
+typedef struct block_t {
+    size_t size;
+    struct block_t *next_free;
+    int is_free;
+    // Other fields...
+} block_t;
+
+extern block_t *free_list_head; // Assuming you have a free list
+extern block_t *heap_start; // Start of your heap
+extern block_t *heap_end; // End of your heap
+
+// Helper functions prototypes
+// int is_in_free_list(block_t *block);
+// block_t *get_next_block(block_t *block);
+// int is_valid_block(block_t *block);
+// int is_within_heap(void *ptr);
+
+int mm_check(void) {
+    int is_consistent = 1;
+
+    // Check 1: Are all blocks in the free list marked as free?
+    for (block_t *current = free_list_head; current != NULL; current = current->next_free) {
+        if (!current->is_free) {
+            fprintf(stderr, "Error: Block in free list not marked as free.\n");
+            is_consistent = 0;
+        }
+    }
+
+    // Check 2: Are there any contiguous free blocks that escaped coalescing?
+    for (block_t *current = heap_start; current < heap_end; current = get_next_block(current)) {
+        if (current->is_free) {
+            block_t *next = get_next_block(current);
+            if (next < heap_end && next->is_free) {
+                fprintf(stderr, "Error: Contiguous free blocks not coalesced.\n");
+                is_consistent = 0;
+            }
+        }
+    }
+
+    // Check 3: Is every free block actually in the free list?
+    for (block_t *current = heap_start; current < heap_end; current = get_next_block(current)) {
+        if (current->is_free && !is_in_free_list(current)) {
+            fprintf(stderr, "Error: Free block not in free list.\n");
+            is_consistent = 0;
+        }
+    }
+
+    // Check 4: Do the pointers in the free list point to valid free blocks?
+    for (block_t *current = free_list_head; current != NULL; current = current->next_free) {
+        if (!is_valid_block(current) || !current->is_free) {
+            fprintf(stderr, "Error: Invalid or non-free block in free list.\n");
+            is_consistent = 0;
+        }
+    }
+
+    // Check 5: Do any allocated blocks overlap?
+    block_t *current = heap_start;
+    while (current < heap_end) {
+        block_t *next = get_next_block(current);
+        if (!current->is_free && next < heap_end && !next->is_free && ((char*)current + current->size > (char*)next)) {
+            fprintf(stderr, "Error: Overlapping allocated blocks.\n");
+            is_consistent = 0;
+        }
+        current = next;
+    }
+
+    // Check 6: Do the pointers in a heap block point to valid heap addresses?
+    for (current = heap_start; current < heap_end; current = get_next_block(current)) {
+        if (!is_within_heap(current)) {
+            fprintf(stderr, "Error: Block pointers not pointing to valid heap addresses.\n");
+            is_consistent = 0;
+        }
+    }
+
+    // Add more checks as per your heap implementation and invariants
+
+    return is_consistent;
+}
+```
+
+### Key Considerations
+
+- **Customization**: Adapt the checks to align with your specific heap management techniques.
+- **Helper Functions**: Functions like `is_in_free_list`, `get_next_block`, `is_valid_block`, and `is_within_heap` need to be defined based on your memory manager's logic.
+- **Error Handling**: The error messages should be informative to aid in debugging.
+- **Efficiency**: Consider the performance implications of these checks, especially if your heap becomes large.
+
+This implementation assumes a certain structure for your heap and free blocks. You may need to adjust the checks and logic based on your actual implementation details.
+
+# Implementation Hurdles
+ - How do we know where the blocks are?
+ - How do we know how big the blocks are?
+ - How do we know which blocks are free?
+ - Remember: can't buffer calls to malloc and free... must deal with them real-time.
+ - Remember: calls to free only takes a pointer, not apointer and a size.
+ - Solution: Need a data structure to store information onthe“blocks”
+    1. Where do I keep this data structure?
+    2. We can't allocate a space for it, that's what we are writing!
+
+# My Block Struct
+~~~shell
+            31 ......  3  2 1 0
+            [   块大小  ][ 0 0 a] (a = 1: 已分配的， a = 0: 空闲的) 块大小包括所有的有效载荷和填充
+   malloc ->[     pred         ](祖先)    ---+
+            [     succ         ](后继)       |
+            [      有效载荷     ]             原来的有效载荷
+            [   只包括已分配的块 ]             |
+            [                  ]         ---+    
+            [------------------]    
+            [-----填充(可选)----]   
+            [------------------]
+            [   块大小  ][ a / f]
+PS: 块大小包括所有的有效载荷（payload）、任何必要的填充（padding）、以及头部（header）和脚部标记（footer）
+~~~
+
+设计思路 显式空闲链表 + LIFO + 首次适配 + 边界标记：
+- **显式数据结构**：在这种方法中，空闲块被组织成一个显式的数据结构，如双向链表。这意味着每个空闲块都显式地包含了指向其他空闲块的指针，而不是隐式地通过块的大小或其他方式来确定。
+
+- **LIFO**: 在这种方法中，当一个内存块被释放时，它被放置在空闲链表的开始处。这意味着最后被释放的块将是第一个被考虑用于新的分配请求的块。由于新释放的块可能仍然在缓存中，这可能提高了内存访问的效率。
+
+- **First Fit** 这是一个常见的内存分配策略，指的是分配器在分配内存时会选择它遇到的第一个足够大的空闲块。结合LIFO顺序，这意味着分配器将首先检查最近释放的块，这些块更可能仍然在CPU的缓存中，从而提高访问速度。
+
+- **释放块的常数时间操作**：在LIFO策略下，释放一个块仅涉及将其放置在空闲链表的头部。这是一个常数时间操作，因为它不依赖于堆的大小或空闲链表的长度。
+
+- **使用边界标记的合并**：边界标记是一种内存管理技术，它在每个内存块的开始和结束处存储信息，如块的大小和分配状态。当一个块被释放时，分配器可以检查相邻的块，如果它们也是空闲的，则可以迅速将它们合并成一个更大的块。这种合并操作同样是常数时间内完成的，因为它仅涉及对几个边界标记的更新，而不需要遍历整个堆或链表。
+
+一些必须的操作：
+1. 操作空闲链表的基本常数和宏
+2. 创建初始空闲链表
+3. 分配块
+4. 双字对齐
+5. 堆扩展
+6. First Fit
+7. 释放块
+8. 放置块
+9. 合并空闲块
+10. `heap checker`
+
+
+# `__LINE__` Macro Usage
+
+The `__LINE__` macro is a built-in preprocessor macro in C and C++ that expands to the current line number in the source code file. It's a part of standard predefined macros that are available in all C and C++ compilers. Here's how you can use it:
+
+### Basic Usage
+
+The `__LINE__` macro is commonly used for debugging purposes, where you want to include the line number in log messages or error reports. For example:
+
+```c
+printf("Error occurred at line number: %d\n", __LINE__);
+```
+
+This will print the line number where this `printf` statement appears in your source code.
+
+### With `assert`
+
+The `__LINE__` macro can be especially useful in conjunction with the `assert` macro for better diagnostics:
+
+```c
+assert(some_condition && "Failure on line " __LINE__);
+```
+
+### Logging
+
+For logging purposes, you might define a macro that includes `__LINE__`:
+
+```c
+#define LOG(msg) printf("[%s:%d] %s\n", __FILE__, __LINE__, msg)
+
+// Usage
+LOG("This is a log message.");
+```
+
+This macro will print the file name and line number along with the log message.
+
+### Error Handling
+
+In error handling, `__LINE__` can be used to provide more detailed information about where an error occurred:
+
+```c
+if (error_condition) {
+    fprintf(stderr, "Error at line %d\n", __LINE__);
+    // Additional error handling
+}
+```
+
+### Limitations and Considerations
+
+- The `__LINE__` macro is replaced by the line number during preprocessing. It cannot be used to determine the line number dynamically at runtime.
+- It's a part of the source code, so if your code gets moved around or line numbers change due to code modification, `__LINE__` will always represent the current line number in the source file.
+- Overusing `__LINE__` (and other debug macros like `__FILE__`) can make the code harder to read and maintain, so it's best used judiciously, particularly in parts of code where tracking location is crucial (like error handling or logging).
